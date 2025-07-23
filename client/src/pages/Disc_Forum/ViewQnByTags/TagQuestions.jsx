834752ee6d8898b80262d695a9da5858961d7787
Vipul Kumar <143587680+Vipul20K@users.noreply.github.com>
2025-07-23 21:37:29 +0530

    Update TagQuestions.jsx

--- client/src/pages/Disc_Forum/ViewQnByTags/TagQuestions.jsx
+++ client/src/pages/Disc_Forum/ViewQnByTags/TagQuestions.jsx
@@ -1,51 +1,10 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import { useParams } from "react-router-dom";
-// import AllQuestions from "../LandingPg/AllQuestions"; 
-// import "./TagQuestions.css";
 
-// const TagQuestions = () => {
-//   const { tag } = useParams();
-//   const [questions, setQuestions] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchQuestionsByTag = async () => {
-//       try {
-//         const res = await axios.get(`/api/v1/forum/questions/tag/${tag}`);
-//         setQuestions(res.data || []);
-//       } catch (err) {
-//         console.error("Error fetching questions by tag:", err);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchQuestionsByTag();
-//   }, [tag]);
-
-//   return (
-//     <div className="tagged-questions-page">
-//       <h2>Questions tagged with <span className="highlight-tag">"{tag}"</span></h2>
-//       {loading ? (
-//         <p>Loading questions...</p>
-//       ) : questions.length > 0 ? (
-//         questions.map((question) => (
-//           <AllQuestions key={question._id} question={question} />
-//         ))
-//       ) : (
-//         <p>No questions found with the tag <strong>{tag}</strong>.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TagQuestions;
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import AllQuestions from "../LandingPg/AllQuestions";
+import axiosInstance from "../../../utils/axiosInstance";
 
 const TagQuestions = () => {
   const { tag } = useParams();
@@ -55,7 +14,7 @@ const TagQuestions = () => {
   useEffect(() => {
     const fetchQuestionsByTag = async () => {
       try {
-        const res = await axios.get(`/api/v1/forum/questions/tag/${tag}`);
+        const res = await axiosInstance.get(`/forum/questions/tag/${tag}`);
         setQuestions(res.data || []);
       } catch (err) {
         console.error("Error fetching questions by tag:", err);
